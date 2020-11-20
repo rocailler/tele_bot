d8a37d1ffaec9ea828a82098a501bbd6bb0625ac
lavarez <63877255+rocailler@users.noreply.github.com>
2020-11-20 17:33:08 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+# requirements = ['']
+setup(name='tele_bot',
+      version='0.0.1',
+      description='Send message to a telegram group easy',
+      author='tombijok',
+      author_email='rocailler@gmail.com',
+      url='https://github.com/rocailler/tele_bot',
+      keywords='telegram, send, message',
+      packages=['tele_bot'],
+     )
